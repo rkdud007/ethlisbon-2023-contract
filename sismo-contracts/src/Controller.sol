347a229e92a7150bc1d58e45acd3ef52388ade15
Pia <76558220+rkdud007@users.noreply.github.com>
2023-05-14 07:02:30 +0100

    sismo

--- sismo-contracts/src/Controller.sol
+++ sismo-contracts/src/Controller.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "sismo-connect-solidity/SismoLib.sol"; // <--- add a Sismo Connect import
 
 /*
@@ -11,58 +10,73 @@ import "sismo-connect-solidity/SismoLib.sol"; // <--- add a Sismo Connect import
  * It will be used to demonstrate how to integrate Sismo Connect
  */
 contract Controller is
-  ERC721,
   SismoConnect // <--- add a Sismo Connect inheritance
 {
   using SismoConnectHelper for SismoConnectVerifiedResult;
 
-  error RegularERC721TransferFromAreNotAllowed();
-  error RegularERC721SafeTransferFromAreNotAllowed();
-
   bytes16 public constant APP_ID = 0xf4977993e52606cfd67b7a1cde717069; // <--- add your appId as a constant
+  uint inactivityTimeout;
+  address controller_guardian_address = 0x00000000;
+  address facts_registry_contract= 0x00000000;
 
   constructor(
-    string memory name,
-    string memory symbol
+    uint memory inactivityTimeout
   )
-    ERC721(name, symbol)
     SismoConnect(APP_ID) // <--- Sismo Connect constructor
-  {}
-
-  function executeInheritance(bytes memory response) public {
-    //can check if the user is a valid user of this group
-    SismoConnectVerifiedResult memory result = verify({
-      responseBytes: response,
-      // we want the user to prove that he owns a Sismo Vault
-      // we are recreating the auth request made in the frontend to be sure that
-      // the proofs provided in the response are valid with respect to this auth request
-      auth: buildAuth({authType: AuthType.VAULT}),
-      // we also want to check if the signed message provided in the response is the signature of the user's address
-      signature: buildSignature({message: abi.encode(msg.sender)})
-    });
-
-    // if the proofs and signed message are valid, we take the userId from the verified result
-    // in this case the userId is the vaultId (since we used AuthType.VAULT in the auth request), the anonymous identifier of a user's vault for a specific app --> vaultId = hash(userVaultSecret, appId)
-    // In this contract, we use this vaultId as the tokenId of the NFT we mint to the user
-    // This way, all proofs that are generated from the same vault will try to mint the same tokenId
-    // So if a user tries to claim twice with a proof from the same vault, the vaultId will be the same and the contract will revert
-    uint256 tokenId = SismoConnectHelper.getUserId(result, AuthType.VAULT);
-
-    //2) do acount inactivity
-
-    // 3) change ownership of the token
-    _mint(msg.sender, tokenId);
+  {
+    setInactivityTimeout(inactivityTimeout);
   }
 
-  function transferFrom(address, address, uint256) public virtual override {
-    revert RegularERC721TransferFromAreNotAllowed();
+  function setInactivityTimeout(uint memory inactivityTimeout) public {
+    _setInactivityTimeout(inactivityTimeout);
   }
 
-  function safeTransferFrom(address, address, uint256) public virtual override {
-    revert RegularERC721SafeTransferFromAreNotAllowed();
+  function _setInactivityTimeout(uint memory _inactivityTimeout) internal {
+    //set inactivity timeout
+    inactivityTimeout = _inactivityTimeout;
   }
 
-  function safeTransferFrom(address, address, uint256, bytes memory) public virtual override {
-    revert RegularERC721SafeTransferFromAreNotAllowed();
-  }
+  function executeInheritance(
+    bytes memory response,
+    uint new_device_key,
+    uint new_admin_key,
+    uint from_block
+  ) public {
+    //get caller address
+    address caller = msg.sender;
+    //get gaurdian address
+    address guardian = controller_guardian_address;
+    // check if the caller is gaurdian and if not return error
+    if (caller != guardian) {
+      revert("Controller: only guardian can execute inheritance");
+    } else {
+      // 1) can check if the user is a valid user of this group
+      SismoConnectVerifiedResult memory result = verify({
+        responseBytes: response,
+        auth: buildAuth({authType: AuthType.VAULT}),
+        signature: buildSignature({message: abi.encode(msg.sender)})
+      });
+
+      // if the proofs and signed message are valid, we take the userId from the verified result
+      // in this case the userId is the vaultId (since we used AuthType.VAULT in the auth request), the anonymous identifier of a user's vault for a specific app --> vaultId = hash(userVaultSecret, appId)
+      // In this contract, we use this vaultId as the tokenId of the NFT we mint to the user
+      // This way, all proofs that are generated from the same vault will try to mint the same tokenId
+      // So if a user tries to claim twice with a proof from the same vault, the vaultId will be the same and the contract will revert
+      uint256 userId = SismoConnectHelper.getUserId(result, AuthType.VAULT);
+      // _mint(msg.sender, tokenId);
+
+      // get facts registry contract
+      // address facts_registry = facts_registry_contract;
+    //   uint256 nonce_before= facts_registry.get_verified_account_nonce(
+    //     facts_registry, contract_address, from_block
+    // );
+
+    //  uint256 nonce_latest = IStarknetFactsRegistry.get_verified_account_nonce(
+    //     facts_registry, contract_address, latest_block_num - 1
+    // );
+    return true;
+
+    };
+
+   
 }
